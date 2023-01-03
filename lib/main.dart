f31e9f32de7d73442dc143fa7b21392f1155d9ba
thisisdishang <dishangkumarrana@gmail.com>
2023-01-03 14:23:38 +0530

    Firebase Configuration Change

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 
-Future<void> main() async{
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
